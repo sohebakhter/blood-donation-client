d25f48c36ed160e4208765bd934ef2e6cc9ea3f7
sohebakhter <sohebakhterbadhan@gmailcom>
2025-12-12 23:18:57 +0600

    navbar customize

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
 import useAuth from "../Hooks/useAuth";
 import { toast } from "react-toastify";
+import logo from "../assets/logo.jpg";
+import { CgProfile } from "react-icons/cg";
 
 const Navbar = () => {
   const { user, signOutUser } = useAuth();
   const links = (
     <>
       <li>
-        <Link to="/">Home</Link>
+        <NavLink to="/">Home</NavLink>
       </li>
       <li>
-        <Link to="/donation-requests">DonationRequests</Link>
+        <NavLink to="/donation-requests">DonationRequests</NavLink>
       </li>
       {user ? (
         <li>
-          <Link to="/funding">Funding</Link>
+          <NavLink to="/funding">Funding</NavLink>
         </li>
       ) : (
         <li>
-          <Link to="/about">About</Link>
+          <NavLink to="/about">About</NavLink>
         </li>
       )}
     </>
@@ -37,13 +39,16 @@ const Navbar = () => {
   return (
     <div className="shadow-sm">
       <div className="max-w-7xl mx-auto navbar bg-base-100">
-        <div className="flex-1">
-          <Link to="/" className="btn btn-ghost text-xl">
-            BloodDonation
+        <div className="flex-1 flex items-center">
+          <img src={logo} alt="" className="w-16 rounded-full" />
+          <Link to="/" className="font-semibold text-red-600 text-5xl">
+            Red Love
           </Link>
         </div>
         <div className="flex gap-2">
-          <ul className="flex items-center gap-2 font-semibold">{links}</ul>
+          <ul className="flex items-center gap-2 font-semibold text-xl">
+            {links}
+          </ul>
           {user ? (
             <>
               <div className="dropdown dropdown-end">
@@ -64,7 +69,7 @@ const Navbar = () => {
                   className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
                 >
                   <li>
-                    <Link to="/dashboard">Dashboard</Link>
+                    <NavLink to="/dashboard">Dashboard</NavLink>
                   </li>
                   <li>
                     <button onClick={handleSignOut}>Logout</button>
@@ -74,8 +79,12 @@ const Navbar = () => {
             </>
           ) : (
             <ul>
-              <li>
-                <Link to="/login" className="btn btn-primary">
+              <li className="flex items-center">
+                <Link
+                  to="/login"
+                  className="btn bg-red-600 text-white font-semibold text-xl"
+                >
+                  <CgProfile className="bg-red-600" />
                   Login
                 </Link>
               </li>
